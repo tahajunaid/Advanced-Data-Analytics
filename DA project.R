9c47e50a1fc7bb93f83229ee4618488c42d25cb6
Taha Junaid <52748060+tahajunaid@users.noreply.github.com>
2021-06-02 13:34:18 +0530

    Add files via upload
    +correlation metrics(p,k,s)
    bagging-RF
    boosting-XGBoost 100%
    stacking (rf+knn+log reg -> gbm) 98%

--- DA project.R
+++ DA project.R
@@ -14,6 +14,7 @@ dim(df_num)
 df_str<-data %>% select_if(~!is.numeric(.x))
 dim(df_str)
 colnames(df_str)
+round(apply(data,2,sd))
 
 #label encoding the categorical variable
 length(unique(df_str$packer_type))
@@ -100,13 +101,6 @@ print(classifier)
 y_pred = predict(classifier, newdata = test)
 confusionMatrix(table(y_pred, test$class))
 
-#Random Forest
-library(randomForest)
-library(caret)
-model = randomForest( class~ ., data=train)
-print(model)
-y_pred = predict(model, newdata = test)
-confusionMatrix(as.factor(test$class),as.factor(round(y_pred)))
 
 #Naive Bayes
 NB=naiveBayes(as.factor(class)~ ., data=train)
@@ -122,8 +116,96 @@ plot(DTmodel)
 y_pred<-predict(DTmodel, test) 
 confusionMatrix(as.factor(test$class),as.factor(y_pred))
 
-#Bagging
+#Ensemble
+##Bagging
+###Random Forest
+library(randomForest)
+library(caret)
+model = randomForest( class~ ., data=train)
+print(model)
+y_pred = predict(model, newdata = test)
+confusionMatrix(as.factor(test$class),as.factor(round(y_pred)))
 
-#boosting
+##Boosting 
+###XGBoost
+library("xgboost")
+
+dtrain <- xgb.DMatrix(label = train$class, data = as.matrix(train))
+dtest <- xgb.DMatrix(label = test$class, data = as.matrix(test))
+xgb_model <- xgboost(data = dtrain, max.depth = 2, eta = 1, nthread = 2, nrounds = 2, objective = "binary:logistic", verbose = 1)
+y_pred<-predict(xgb_model,dtest)
+confusionMatrix(as.factor(test$class),as.factor(round(y_pred)))
+
+##Stacking
+row.names(train) <- NULL
+fitControl <- trainControl(
+  method = "cv",
+  number = 10,
+  savePredictions = 'final', # To save out of fold predictions for best parameter combinantions
+  classProbs = T # To save the class probabilities of the out of fold predictions
+)
+#Defining the predictors and outcome
+predictors<-colnames(train[-57])
+outcomeName<-"class"
+model_rf<-train(train[,predictors],train[,outcomeName],method='rf',trControl=fitControl,tuneLength=3)
+#Predicting using random forest model
+test$pred_rf<-predict(object = model_rf,test[,predictors])
+
+#Checking the accuracy of the random forest model
+confusionMatrix(as.factor(test$class),as.factor(round(test$pred_rf)))
+#Training the knn model
+model_knn<-train(train[,predictors],train[,outcomeName],method='knn',trControl=fitControl,tuneLength=3)
+
+#Predicting using knn model
+test$pred_knn<-predict(object = model_knn,test[,predictors])
+
+#Checking the accuracy of the random forest model
+confusionMatrix(as.factor(test$class),as.factor(round(test$pred_knn)))
+
+#Training the Logistic regression model
+model_lr<-train(train[,predictors],train[,outcomeName],method='glm',trControl=fitControl,tuneLength=3)
+
+#Predicting using lr
+test$pred_lr<-predict(object = model_lr,test[,predictors])
+
+#Checking the accuracy of the lr
+confusionMatrix(as.factor(test$class),as.factor(round(test$pred_lr)))
 
 
+#Taking average of predictions
+test$pred_avg<-(test$pred_rf+test$pred_knn+test$pred_lr)/3
+
+#Splitting into binary classes at 0.5
+test$pred_avg<-as.factor(ifelse(test$pred_avg>0.5,'Y','N'))
+
+#The majority vote
+test$pred_majority<-as.factor(ifelse(test$pred_rf>=0.5 & test$pred_knn>=0.5,'Y',
+                                     ifelse(test$pred_rf>=0.5 & test$pred_lr>=0.5,'Y',
+                                            ifelse(test$pred_knn>=0.5 & test$pred_lr>=0.5,'Y','N'))))
+
+#Taking weighted average of predictions
+test$pred_weighted_avg<-(test$pred_rf*0.25)+(test$pred_knn*0.25)+(test$pred_lr*0.5)
+
+#Splitting into binary classes at 0.5
+test$pred_weighted_avg<-as.factor(ifelse(test$pred_weighted_avg>0.5,'Y','N'))
+
+#Predicting the out of fold prediction probabilities for training data
+train$OOF_pred_rf<-model_rf$pred$obs[order(model_rf$pred$rowIndex)]
+train$OOF_pred_knn<-model_knn$pred$obs[order(model_knn$pred$rowIndex)]
+train$OOF_pred_lr<-model_lr$pred$obs[order(model_lr$pred$rowIndex)]
+#Predicting probabilities for the test data
+test$OOF_pred_rf<-predict(model_rf,test[predictors])
+test$OOF_pred_knn<-predict(model_knn,test[predictors])
+test$OOF_pred_lr<-predict(model_lr,test[predictors])
+
+
+#Predictors for top layer models 
+predictors_top<-c('OOF_pred_rf','OOF_pred_knn','OOF_pred_lr') 
+
+#GBM as top layer model 
+model_gbm<-  train(train[,predictors_top],train[,outcomeName],method='gbm',trControl=fitControl,tuneLength=3)
+
+#predict using GBM top layer model
+test$gbm_stacked<-predict(model_gbm,test[,predictors_top])
+
+confusionMatrix(as.factor(test$class),as.factor(round(test$gbm_stacked)))
